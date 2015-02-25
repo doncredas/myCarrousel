f297ce1a84b5dfe4a3651a132445d68494ce6011
doncredas <doncredas@gmail.com>
2015-02-25 12:04:13 +0100

    The myCarrousel plugin (To be optimized) with only the automatic slideshow

--- myCarrousel.js
+++ myCarrousel.js
@@ -1,3 +1,30 @@
 /**
  * Created by doncredas on 25/02/15.
  */
+function theCarrousel(config){
+    var myCarrousel = $("#myCarrousel"),
+        img = $("#myCarrousel img"),
+        imgLength = img.length - 1,
+        i = 0,
+        currentImg = img.eq(i);
+    img.hide();
+
+    currentImg.css("display", "block");
+
+    function slideImg(slideTime){
+        setTimeout(function () {
+            if(i == imgLength){
+                i = 0;
+            }else{
+                i++;
+            }
+            currentImg.css('display', 'none');
+            currentImg = img.eq(i);
+            currentImg.css('display', 'block');
+            slideImg(slideTime);
+        } , slideTime);
+    }
+
+    var time = config.time;
+    slideImg(time);
+}
